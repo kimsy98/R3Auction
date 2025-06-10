7617bb7180529d94365ca541d88892e282d4b233
kimsy98 <gkstjeo17@naver.com>
2025-06-10 19:20:43 +0900

    멀티 스레드 배치로 성능 향상

--- user/src/main/java/com/r3a/user/config/SecurityConfig.java
+++ user/src/main/java/com/r3a/user/config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/", "/api/v1/batch/**").permitAll()
                         .requestMatchers("/reissue").permitAll()
                         .anyRequest().authenticated());
 
